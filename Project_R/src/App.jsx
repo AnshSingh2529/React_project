08b18586b6fcf837fe1ff9d649c443fe37117958
Ansh Singh <anshsingh2529@gmail.com>
2024-03-12 00:54:43 +0530

    start adding data..

---
+++ Project_R/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <div>
+    A blog app with appwrite
+    </div>
+  )
+}
+
+export default App
