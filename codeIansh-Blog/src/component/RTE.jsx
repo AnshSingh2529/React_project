c69f9e1acbdaab264ed7f3d4ebe1f7880f23ac1f
Ansh Singh <anshsingh2529@gmail.com>
2024-03-31 21:06:27 +0530

    working on Real Time Editor Component

---
+++ codeIansh-Blog/src/component/RTE.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Editor} from '@tinymce/tinymce-react';
+import {Controller} from 'react-hook-form';
+
+
+function RTE({name, control, label, defaultValue=""}) {
+  return (
+    <div>test</div>
+  )
+}
+
+export default RTE
