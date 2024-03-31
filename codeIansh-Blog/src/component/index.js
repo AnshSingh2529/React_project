743b6be0bc93f086009172992b8d73bdb396fa5f
Ansh Singh <anshsingh2529@gmail.com>
2024-04-01 00:32:09 +0530

    working on RTE and PostForm

--- codeIansh-Blog/src/component/index.js
+++ codeIansh-Blog/src/component/index.js
@@ -5,6 +5,8 @@ import Logo from "./Logo";
 import Logoutbtn from "./Header/Logoutbtn";
 import Button from './Header/Button';
 import Input from './Input';
+import Select from './Select';
+import RTE from "./RTE";
 
 
-export {Header, Footer, Container, Logo, Button, Logoutbtn, Input }
+export {Header, Footer, Container, Logo, Button, Logoutbtn, Input, RTE, Select }
