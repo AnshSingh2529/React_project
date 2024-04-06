a149d7fb5c64c26f22d7c8676e838d16b46a24d4
Ansh Singh <anshsingh2529@gmail.com>
2024-04-06 22:21:49 +0530

    Chenge API key in RTE

--- src/component/RTE.jsx
+++ src/component/RTE.jsx
@@ -13,7 +13,7 @@ function RTE({name, control, label, defaultValue=""}) {
         control={control}
         render={ ({field: {onChange}})=> (
           <Editor 
-          apiKey='42yqmns3va6cxupl951wtqp4itt0a3n19iy5jmlbzli3653h'
+          apiKey='gs2onds48sg3i2bxunsv15qp5djfmqj1czj9le8t23gg9o42'
           initialValue={defaultValue}
           init={
             {
